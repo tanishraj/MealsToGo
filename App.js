0929758a87829937f48560c281c01a94f9b8006f
tanishrajkumar <tanishraj.k@bsfdv.com>
2024-03-13 07:33:43 +0400

    fix: lint fixes

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { ThemeProvider } from 'styled-components/native';
-import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
-import {  useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 import { theme } from "./src/infrastructure/theme";
 
