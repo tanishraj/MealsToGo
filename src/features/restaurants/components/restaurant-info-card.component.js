3aa42bb337b10936f20305d27bff331854afa32c
tanishrajkumar <tanishraj.k@bsfdv.com>
2024-03-14 14:59:24 +0400

    feat: Flat list implementation

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,65 +1,28 @@
 import React from "react";
-import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
-import { Card, Text } from "react-native-paper";
+import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star";
 import openIcon from "../../../../assets/open-icon";
 import closeIcon from "../../../../assets/close-icon";
 import dinerIcon from "../../../../assets/diner-icon";
-import { Image } from "react-native";
 
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[2]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.colors.text.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const CardContent = styled(Card.Content)`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Info = styled.View``;
-
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-  color: ${(props) => props.theme.colors.text.secondary};
-`;
-
-const Ratings = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: space-between;
-  justify-content: space-between;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-  gap: ${(props) => props.theme.space[2]};
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  CardContent,
+  Info,
+  Section,
+  SectionEnd,
+  Ratings,
+  Address,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     restaurantId = "1",
     name = "Kha Lo Friends",
     address = "123, Gaighat Ballia",
-    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    icon = dinerIcon,
     photos = ["https://static.actu.fr/uploads/2022/04/abendbrot-939435.jpg"],
     isOpenNow = true,
     rating = 4,
@@ -74,7 +37,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <CardContent>
         <Info>
           <Section>
-            <Title>{name}</Title>
+            <Text variant="label">{name}</Text>
             <SectionEnd>
               {isOpenNow && <SvgXml xml={openIcon} width={30} height={30} />}
               {isClosedTemporarily && (
@@ -92,7 +55,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           <Section>
             <Address>{address}</Address>
             <SectionEnd>
-              <SvgXml xml={dinerIcon} width={30} height={30} />
+              <SvgXml xml={icon} width={30} height={30} />
             </SectionEnd>
           </Section>
         </Info>
--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
-import styled from "styled-components/native";
+import { FlatList } from "react-native";
+import { SafeArea, SearchContainer } from "./restaurant.styles";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[2]};
-`;
-
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[2]};
-`;
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const RestaurantScreen = () => {
   return (
@@ -23,9 +12,12 @@ export const RestaurantScreen = () => {
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.toString()}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
